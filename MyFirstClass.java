039674ba49f74bcf4ff63eef1a24b46e565e7817
林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
2025-04-10 02:50:32 +0800

    feat: Implement hello world tradition
    Signed-off-by: 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>

--- MyFirstClass.java
+++ MyFirstClass.java
@@ -3,4 +3,7 @@
 // SPDX-FileCopyrightText: 2025 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
 // SPDX-License-Identifier: MIT
 public class MyFirstClass {
+    public static void main(String... args) {
+        System.out.println("Hello, World!");
+    }
 }
